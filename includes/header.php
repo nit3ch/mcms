ecce4f2a95cdc98fc5a5b218e177f33e8ff4344b
Nitesh Kumar <creative2all@gmail.com>
2012-02-16 16:17:35 +0530

    sending all files

---
+++ includes/header.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+  <link rel="stylesheet" href="/cms/stylesheets/css/blueprint/screen.css" type="text/css" media="screen, projection">
+  <link rel="stylesheet" href="/cms/stylesheets/css/blueprint/print.css" type="text/css" media="print"> 
+  <!--[if lt IE 8]>
+    <link rel="stylesheet" href="css/blueprint/ie.css" type="text/css" media="screen, projection">
+  <![endif]-->
+	</head>
+		<title> mycms </title>
+			<link rel="stylesheet" type="text/css" href="stylesheets/mystyles.css"/>
+		<body>
+			
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+include '/var/www/cms/includes/header.php';
+?>
+    <div class="container">
+    <div class="span-24">
+        <h1> website-name</h1>
+    </div>
+
+    <div class="span-4">
+        The first column
+    </div>
+    <div class="span-15">
+        The center column
+    </div>
+    <div class="span-4 last">
+        The last column
+    </div>
+    <div class="span-24">
+        The footer
+    </div>
+  </div>
